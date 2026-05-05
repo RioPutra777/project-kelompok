82f31b06c78e8cbf3b1c2d5ca5b93e7b9d0b9ea1
RioPutra777 <alohario2004@gmail.com>
2026-05-05 17:28:08 +0800

    tugas-2

--- my-app/app/learn/page.tsx
+++ my-app/app/learn/page.tsx
@@ -1,14 +1,39 @@
-export default function LearnPage() {
-  return (
-    <div className="bg-pink-500">
-      <div className='bg-orange-300'>
-        <img/>
-        <p>Design</p>
-      </div>
-      <div>
-        <h1>FIGMA</h1>
-        <p>8 hours ago</p>
-      </div>
+export default function learnPage() {
+    return(
+    <div className="bg-white h-screen flex gap-10 justify-center items-center">
+        <div className="bg-purple-300 h-72 w-94 flex flex-col justify-between p-8 rounded-4xl">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p className="text-black">Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl text-black font-bold">Figma</h1>
+                <p className="text-black"> 8 hours ago</p>
+            </div>
+        </div>
+
+        <div className="bg-yellow-300 h-72 w-94 flex flex-col justify-between p-8 rounded-4xl">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p className="text-black">Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl text-black font-bold">Python</h1>
+                <p className="text-black"> 3 days ago</p>
+            </div>
+        </div>
+
+        <div className="bg-green-300 h-72 w-94 flex flex-col justify-between p-8 rounded-4xl">
+            <div className="flex gap-4">
+                <img src="/palette.svg"/>
+                <p className="text-black">Design</p>
+            </div>
+            <div>
+                <h1 className="text-4xl text-black font-bold">Sketch</h1>
+                <p className="text-black"> 6 days ago</p>
+            </div>
+        </div>
     </div>
-  )
+    )
+
 }
