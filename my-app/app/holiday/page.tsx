1ba05ae53413bb3531a45d91668e72cd6332571c
RioPutra777 <alohario2004@gmail.com>
2026-05-07 11:56:36 +0800

    quiz

---
+++ my-app/app/holiday/page.tsx
@@ -0,0 +1,17 @@
+export default function takepage() {
+    return
+    <div className="bg-gray-300 h-screen flex gap-5 justify-center items-center flex-col text-black">
+       <div className="flex gap-4 justify center">
+
+        <div className="bg-white h-110 w-80 flex p-5 rounded-4x1 flex-col gap-4">
+
+            <div className="flex justify-center">
+                 <img src="a1.jpg className="h-60 w-75 items-center rounded-4x1
+                 />
+
+            </div>
+            
+        </div>
+        </div> 
+    </div>
+}
