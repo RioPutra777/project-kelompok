f197c23a7bc410f579f1262485a75e7061f0a92d
RioPutra777 <alohario2004@gmail.com>
2026-06-04 12:26:03 +0800

    navbar

---
+++ my-app/app/navbar/page.tsx
@@ -0,0 +1,10 @@
+export default function NavbarPage () {
+    return (
+        <div className="h-screen flex justify center item-center">
+        <div className="bg-gray-50 w-80 p-2 rounded-4xl ">
+            <img src={"public/layout-dashboard.psvg"} className="h-72 w-full rounded-4xl" />
+            
+        </div>
+        </div>
+    )
+} 
